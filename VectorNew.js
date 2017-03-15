6268a05befb1f8b7c9606ef921662744b468411b
seonkyoo <skyoon5423@gmail.com>
2017-03-14 23:16:02 -0400

    enkf implementation, but not working well

--- VectorNew.js
+++ VectorNew.js
@@ -1,3 +1,30 @@
+// Gaussain variable generator using Box-Muller transform.
+function randn_bm() {
+    var u = 1 - Math.random(); // Subtraction to flip [0, 1) to (0, 1].
+    var v = 1 - Math.random();
+    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )/2;
+}
+
+
+function histo(Ens){
+		// Histogram of Stifnesses 
+		var x = [];
+		for (var i = 0; i < Ne; i ++) {
+			x[i] = Math.log(Ens[i].stiffness);
+		}
+
+		var data = [
+		  {
+		    x: x,
+		    type: 'histogram',
+			marker: {
+		    color: 'rgba(100,250,100,0.7)',
+			},
+		  }
+		];
+		Plotly.newPlot('histo', data);
+}
+
 
 function Vector(x,y){
 	var v = Object.create(Methods);
